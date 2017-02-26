9d5063973e13f6d0f05848aa197113928d88586c
shawnpflueger <shawnpflueger@gmail.com>
2017-02-26 16:41:31 -0600

    Spring Initialzr Beginnings
    Downloaded an Initialzr project and commented out the dependencies in
    the pom that I think I will eventually use, but aren't necssary to get
    started.

---
+++ src/test/java/org/spisiss/shuffler/DominionShufflerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.spisiss.shuffler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DominionShufflerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
